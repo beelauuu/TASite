624c0c09090d71ad16d75fe4bd2971afb321d7b3
beelauuu <blau1@terpmail.umd.edu>
2024-02-22 14:43:14 -0500

    quiz

--- src/components/Content.js
+++ src/components/Content.js
@@ -168,6 +168,14 @@ function Content({ isNightMode }) {
             >
               Slides
             </button>
+            <button
+              type="button"
+              onClick={() => handleButtonClick('https://github.com/beelauuu/TASite/raw/main/public/discussion/Quiz 2_removed.pdf')}
+              style={{ marginLeft: '10px' }}
+              className={`btn ${isNightMode ? 'btn-light' : 'btn-dark'}`}
+            >
+              Code
+            </button>
           </div>
           
         </div>
