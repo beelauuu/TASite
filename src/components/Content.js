b41ae233fc37aed81f7a8fc59fe896d1cb05a60a
beelauuu <blau1@terpmail.umd.edu>
2024-04-02 13:14:17 -0400

    stuff

--- src/components/Content.js
+++ src/components/Content.js
@@ -359,6 +359,14 @@ function Content({ isNightMode }) {
               >
                 Slides
               </button>
+              <button
+              type="button"
+              onClick={() => handleButtonClick('https://github.com/beelauuu/TASite/raw/main/public/discussion/DiscussionSeventeen.zip')}
+              style={{ marginLeft: '10px' }}
+              className={`btn ${isNightMode ? 'btn-light' : 'btn-dark'}`}
+              >
+              Code
+              </button>
             </div>
 
             {/* Discussion 18*/}
