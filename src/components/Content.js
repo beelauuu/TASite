bcdf57eb32af009a1e12d46bd65aec8cfdd0d204
beelauuu <blau1@terpmail.umd.edu>
2024-05-06 23:55:17 -0400

    stuff

--- src/components/Content.js
+++ src/components/Content.js
@@ -504,6 +504,20 @@ function Content({ isNightMode }) {
                 Slides
               </button>
             </div>
+
+            <div className={`wrapper mt-1 ${isNightMode ? 'night-mode-text' : 'day-mode-text'}`}>
+              <p style={{ display: 'inline' }} className={`week ${isNightMode ? 'night-mode-text' : 'day-mode-text'}`}>
+                Discussion 27:
+              </p>
+              <button
+                type="button"
+                onClick={() => handleButtonClick('https://beelauuu.github.io/slides/docs/50724')}
+                style={{ marginLeft: '10px' }}
+                className={`btn ${isNightMode ? 'btn-light' : 'btn-dark'}`}
+              >
+                Slides
+              </button>
+            </div>
         </div>
       </div>
     </div>
